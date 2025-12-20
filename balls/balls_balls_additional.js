01895c0f4c21a402743db3a21dc17b4f892e29da
plaaosert <terrariandallas@gmail.com>
2025-12-20 22:08:33 +0000

    more additional balls

--- balls/balls_balls_additional.js
+++ balls/balls_balls_additional.js
@@ -141,6 +141,219 @@ class AStickBall extends WeaponBall {
 }
 
 
+class ThirteenSticksBall extends WeaponBall {
+    static ball_name = "13 Sticks";
+
+    constructor(board, mass, radius, colour, bounce_factor, friction_factor, player, level, reversed) {
+        super(board, mass, radius, colour, bounce_factor, friction_factor, player, level, reversed);
+    
+        this.name = "13 Sticks";
+        this.description_brief = "Has 13 sticks that do 75 damage.";
+        this.level_description = "This ball has no levelup effect.";
+        this.max_level_description = "This ball has no awakening effect.";
+        this.quote = "I think we're going too far.";
+
+        this.weapon_data = new Array(13).fill("").map(_ => new BallWeapon(1, "stick", [
+                {pos: new Vector2(88, 66), radius: 10},
+                {pos: new Vector2(68, 74), radius: 10},
+                {pos: new Vector2(48, 74), radius: 10},
+                {pos: new Vector2(32, 72), radius: 10},
+                {pos: new Vector2(16, 68), radius: 10},
+            ]))
+
+        this.speeds_range = [60, 160]
+
+        this.speeds = new Array(this.weapon_data.length).fill(0).map(_ => random_float(...this.speeds_range, this.board.random));
+        
+        for (let i=0; i<this.weapon_data.length; i++) {
+            if (i % 2 == 1) {
+                this.reverse_weapon(i);
+            }
+        }
+
+        this.damage_base = 75;
+    }
+
+    weapon_step(board, time_delta) {
+        // rotate the weapon
+        for (let i=0; i<this.weapon_data.length; i++) {
+            this.rotate_weapon(i, this.speeds[i] * time_delta);
+        }
+    }
+
+    hit_other(other, with_weapon_index) {
+        let dmg = this.damage_base;
+        let result = super.hit_other(other, with_weapon_index, dmg);
+
+        result.snd = "strongpunch";
+
+        return result;
+    }
+
+    render_stats(canvas, ctx, x_anchor, y_anchor) {
+        this.start_writing_desc(ctx, x_anchor, y_anchor);
+
+        this.write_desc_line(
+            `Damage: ${this.damage_base.toFixed(2)}`
+        )
+        this.write_desc_line(
+            `Sticks: Lots (${this.weapon_data.length} to be exact)`
+        )
+    }
+}
+
+
+class GreatsordBall extends WeaponBall {
+    static ball_name = "GREATSORD";
+
+    constructor(board, mass, radius, colour, bounce_factor, friction_factor, player, level, reversed) {
+        super(board, mass, radius, colour, bounce_factor, friction_factor, player, level, reversed);
+    
+        this.name = "GREATSORD";
+        this.description_brief = "Has a GREATSORD that does 50 damage.";
+        this.level_description = "This ball has no levelup effect.";
+        this.max_level_description = "This ball has no awakening effect.";
+        this.quote = "Requested by @TheGlimGuy.";
+
+        this.weapon_data = [
+            new BallWeapon(2, "SORD_berserk", [
+                {pos: new Vector2(100, 58), radius: 12},
+                {pos: new Vector2(80, 64), radius: 16},
+                {pos: new Vector2(64, 64), radius: 16},
+                {pos: new Vector2(48, 64), radius: 16},
+                {pos: new Vector2(32, 64), radius: 16},
+                {pos: new Vector2(16, 64), radius: 16},
+            ])
+        ];
+
+        this.damage_base = 50;
+        this.speed = 20;
+        
+        this.custom_parry_sound = "CLANG";
+        this.custom_projectile_parry_sound = "CLANG";
+    }
+
+    weapon_step(board, time_delta) {
+        // rotate the weapon
+        this.rotate_weapon(0, this.speed * time_delta);
+    }
+
+    hit_other(other, with_weapon_index) {
+        let dmg = this.damage_base;
+        let result = super.hit_other(other, with_weapon_index, dmg);
+
+        result.snd = "CLANG";
+
+        return result;
+    }
+
+    render_stats(canvas, ctx, x_anchor, y_anchor) {
+        this.start_writing_desc(ctx, x_anchor, y_anchor);
+
+        this.write_desc_line(
+            `Damage: ${this.damage_base.toFixed(2)}`
+        )
+        this.write_desc_line(
+            `SORD: Great`
+        )
+    }
+}
+
+
+class ExtralongswordBall extends WeaponBall {
+    static ball_name = "Extralongsword";
+
+    constructor(board, mass, radius, colour, bounce_factor, friction_factor, player, level, reversed) {
+        super(board, mass, radius, colour, bounce_factor, friction_factor, player, level, reversed);
+    
+        this.name = "Extralongsword";
+        this.description_brief = "Has an extralongsword that does 10 damage and is also on fire.";
+        this.level_description = "This ball has no levelup effect.";
+        this.max_level_description = "This ball has no awakening effect.";
+        this.quote = "Suggested by @_Olar_.";
+
+        this.weapon_data = new Array(1).fill("").map(_ => new BallWeapon(3, "LONGSORD", [
+            {pos: new Vector2(116, 72), radius: 8},
+            {pos: new Vector2(100, 72), radius: 8},
+            {pos: new Vector2(80, 74), radius: 8},
+            {pos: new Vector2(64, 74), radius: 8},
+            {pos: new Vector2(48, 74), radius: 8},
+            {pos: new Vector2(32, 72), radius: 8},
+            {pos: new Vector2(16, 68), radius: 8},
+        ]))
+
+        this.damage_base = 10
+        this.speeds_range = [160, 160];
+
+        this.speeds = new Array(this.weapon_data.length).fill(0).map(_ => random_float(...this.speeds_range, this.board.random));
+        
+        for (let i=0; i<this.weapon_data.length; i++) {
+            if (i % 2 == 1) {
+                this.reverse_weapon(i);
+            }
+        }
+
+        this.explosions_num = 12;
+        this.explosions_range_per_sord = [0,4];
+        this.explosions_delay_max = 0.02;
+        this.explosions_delay = this.explosions_delay_max;
+    }
+
+    weapon_step(board, time_delta) {
+        // rotate the weapon
+        for (let i=0; i<this.weapon_data.length; i++) {
+            this.rotate_weapon(i, this.speeds[i] * time_delta);
+        }
+
+        this.explosions_delay -= time_delta;
+        while (this.explosions_delay < 0) {
+            this.explosions_delay += this.explosions_delay_max;
+
+            for (let i=0; i<this.explosions_num; i++) {
+                for (let wp=0; wp<this.weapon_data.length; wp++) {
+                    let wp_offset = this.get_weapon_offset(wp);
+                    let hitboxes = this.get_hitboxes_offsets(wp);
+
+                    let times = random_int(...this.explosions_range_per_sord, this.board.random);
+                    for (let n=0; n<times; n++) {
+                        let hitbox_index = random_int(0, hitboxes.length, this.board.random);
+
+                        let maxn = this.weapon_data[wp].hitboxes[hitbox_index].radius * this.weapon_data[wp].size_multiplier;
+                        let variance = random_on_circle(random_float(0, maxn, this.board.random), this.board.random);
+
+                        let pos = this.position.add(wp_offset.add(hitboxes[hitbox_index])).add(variance);
+                        this.board.spawn_particle(new Particle(
+                            pos, random_float(0, Math.PI * 2, this.board.random), 0.1 * (this.weapon_data[wp].size_multiplier / WEAPON_SIZE_MULTIPLIER), entity_sprites.get("explosion"), 24, 3, false, 0, true
+                        ), pos);
+                    }
+                }
+            }
+        }
+    }
+
+    hit_other(other, with_weapon_index) {
+        let dmg = this.damage_base;
+        let result = super.hit_other(other, with_weapon_index, dmg);
+
+        return result;
+    }
+
+    render_stats(canvas, ctx, x_anchor, y_anchor) {
+        this.start_writing_desc(ctx, x_anchor, y_anchor);
+
+        this.write_desc_line(
+            `Damage: ${this.damage_base.toFixed(2)}`
+        )
+        this.write_desc_line(
+            `Longswords: 1`
+        )
+        this.write_desc_line(
+            `But also very long`
+        )
+    }
+}
+
+
 let additional_selectable_balls = [
-    ThirteenLongswordsBall, AStickBall
+    ThirteenLongswordsBall, AStickBall, ThirteenSticksBall, GreatsordBall, ExtralongswordBall
 ]
